3546b7ccdd5cc1fbdc65ee9e52224edc10cfbef5
David Liman <limanoit@gmail.com>
2022-02-21 10:09:18 -0800

    extend protocol to read Array/PGobject from result set

--- example/src/user.clj
+++ example/src/user.clj
@@ -1,11 +1,40 @@
 (ns user
   (:require [naptime.db :as db]
             [clojure.pprint :as pp]
+            [jsonista.core :as json]
             [next.jdbc.sql :as sql]
+            [next.jdbc.result-set :as result-set]
             [conman.core :as conman]
-            [mount.core :as mount :refer [defstate]]))
+            [mount.core :as mount :refer [defstate]])
+  (:import [org.postgresql.util PGobject]
+           (java.sql PreparedStatement Array)
+           (clojure.lang IPersistentVector IPersistentMap)))
 
-(def pool-spec {:jdbc-url "jdbc:postgresql://localhost/naptime?user=postgres"})
+(def pool-spec {:jdbc-url "jdbc:postgresql://localhost:1234/studio?user=postgres&password=tpJ4dAmQU52mdL5"})
+
+(defn ->pgobject [type value]
+  (doto (PGobject.)
+    (.setType type)
+    (.setValue (json/write-value-as-string value))))
+
+(defn jsonb [value]
+  (->pgobject "JSONB" value))
+
+(defn <-pgobject [^PGobject v]
+  (let [type (.getType v)
+        value (.getValue v)]
+    (if (#{"jsonb" "json"} type)
+      (json/read-value value)
+      value)))
+
+(extend-protocol result-set/ReadableColumn
+  Array
+  (read-column-by-index [val _ _]
+    (into [] (.getArray val)))
+
+  PGobject
+  (read-column-by-label [x _] (<-pgobject x))
+  (read-column-by-index [x _2 _3] (<-pgobject x)))
 
 (defstate ^:dynamic *db*
   :start (conman/connect! pool-spec)
@@ -14,5 +43,11 @@
 (defstate queries
   :start (db/load-queries :postgres))
 
-(defn get-tables []
-  (db/query *db* queries :all-tables))
+(defn all-tables [db]
+  (db/query db queries :all-tables))
+
+(defn many-to-one [db]
+  (db/query db queries :many-to-one))
+
+(clojure.pprint/pprint (many-to-one *db*))
+((naptime/handler *db*) (mock/request :get naptime/logical-operators-qs))
