a69dc5a7cc24ba0b5525f37fe9b9ff65b212f504
Daniel Fitzpatrick <crinklywrappr@pm.me>
2022-05-01 23:38:47 -0500

    add password

--- example/src/user.clj
+++ example/src/user.clj
@@ -5,7 +5,7 @@
             [conman.core :as conman]
             [mount.core :as mount :refer [defstate]]))
 
-(def pool-spec {:jdbc-url "jdbc:postgresql://localhost/university?user=postgres"})
+(def pool-spec {:jdbc-url "jdbc:postgresql://localhost/university?user=postgres&password=password"})
 
 (defstate ^:dynamic *db*
   :start (conman/connect! pool-spec)
