20ecf1f05d5c9e1e0c2b3533381a0ab13ff9cb15
Daniel Fitzpatrick <crinklywrappr@pm.me>
2022-05-26 17:35:19 -0500

    reverse memoize/ttl change

--- src/naptime/core.clj
+++ src/naptime/core.clj
@@ -1,28 +1,22 @@
 (ns naptime.core
   (:require [naptime.layers.meta :as meta]
             [naptime.layers.query :as query]
-            [naptime.layers.graph :as graph]
-            [clojure.core.memoize :as memo]))
+            [naptime.layers.graph :as graph]))
 
 ;; don't need this yet but
 ;; host->database->schema->table
 
 (defn init-meta
-  ([datasource]
-   (init-meta datasource {}))
-  ([datasource
-    {:keys [ttl]
-     :or {ttl (* 6 3600000)}
-     :as opts}]
-   (let [queries (meta/load-queries :postgres)]
-     {:datasource datasource
-      :meta/all-tables (memo/ttl (partial meta/all-tables datasource queries) :ttl/threshold ttl)
-      :meta/all-columns (memo/ttl (partial meta/all-columns datasource queries) :ttl/threshold ttl)
-      :meta/many-to-one (memo/ttl (partial meta/many-to-one datasource queries) :ttl/threshold ttl)
-      :meta/all-primary-keys (memo/ttl (partial meta/all-primary-keys datasource queries) :ttl/threshold ttl)
-      :meta/all-stored-procedures (memo/ttl (partial meta/all-stored-procedures datasource queries) :ttl/threshold ttl)
-      :meta/primary-and-foreign-keys-referenced-in-views
-      (memo/ttl (partial meta/primary-and-foreign-keys-referenced-in-views datasource queries) :ttl/threshold ttl)})))
+  [datasource]
+  (let [queries (meta/load-queries :postgres)]
+    {:datasource datasource
+     :meta/all-tables (memoize (partial meta/all-tables datasource queries))
+     :meta/all-columns (memoize (partial meta/all-columns datasource queries))
+     :meta/many-to-one (memoize (partial meta/many-to-one datasource queries))
+     :meta/all-primary-keys (memoize (partial meta/all-primary-keys datasource queries))
+     :meta/all-stored-procedures (memoize (partial meta/all-stored-procedures datasource queries))
+     :meta/primary-and-foreign-keys-referenced-in-views
+     (memoize (partial meta/primary-and-foreign-keys-referenced-in-views datasource queries))}))
 
 (defn init-queries [meta]
   (-> meta query/get-create-fns))
